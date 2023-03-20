2fc63d97473c5bbf851374ad17d0a04e0f80ae23
mirandajaorenzo26 <119150319+mirandajaorenzo26@users.noreply.github.com>
2023-03-20 21:21:13 +0800

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  private albumInfo;
-  
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+    this._productService.getAlbum(1).subscribe(response =>
+    this.albumInfo = response)
   }
 
 }
