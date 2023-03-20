761c371bc587e32bb527a77109d29f2d39fd9e60
mirandajaorenzo26 <119150319+mirandajaorenzo26@users.noreply.github.com>
2023-03-20 21:39:10 +0800

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
